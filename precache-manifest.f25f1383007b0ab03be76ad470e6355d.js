a2d2429618bffb05bc0df3c489e111eadef77156
bycristhian <cristhian.2002.0@gmail.com>
2020-11-11 19:51:10 -0500

    Updates

---
+++ precache-manifest.f25f1383007b0ab03be76ad470e6355d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a048f65ef907fbb5de56f50ba7f4ad1",
+    "url": "/zodiacod/index.html"
+  },
+  {
+    "revision": "c1e9471ddfa15d5e5b0c",
+    "url": "/zodiacod/static/css/main.98d6f055.chunk.css"
+  },
+  {
+    "revision": "e706ea65c8e2e5e9540d",
+    "url": "/zodiacod/static/js/2.f1e5f4b3.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/zodiacod/static/js/2.f1e5f4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e9471ddfa15d5e5b0c",
+    "url": "/zodiacod/static/js/main.c30e49d1.chunk.js"
+  },
+  {
+    "revision": "1d20dd6fd8b386efe97e",
+    "url": "/zodiacod/static/js/runtime-main.1ace7d6b.js"
+  },
+  {
+    "revision": "23597659f1407432a01932bfa4509485",
+    "url": "/zodiacod/static/media/background.23597659.jpg"
+  },
+  {
+    "revision": "ada5198cfdc61891d77b5a095a9b8d85",
+    "url": "/zodiacod/static/media/background_comp.ada5198c.jpg"
+  }
+]);
