3a4024ec9c866a9cd1180815a956ed4ce78d48fa
bycristhian <cristhian.2002.0@gmail.com>
2020-11-13 19:00:59 -0500

    The configuration for deploy in github pages was created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import MySign from './pages/MySign';
 
 const App = () => {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/my-sign/" component={MySign} />
